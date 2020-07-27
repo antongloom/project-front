3defc406cfc3eeb0b5f340cc33b48fcd5b0db8ed
Anton <kleshnev.aa@infotek.ru>
2020-07-27 16:37:49 +0300

    Новое

--- src/views/DisplayCurriculum.vue
+++ src/views/DisplayCurriculum.vue
@@ -19,8 +19,9 @@
 			    <v-expansion-panel
 			    	v-for="(section, index) in selectedCurriculum.sections"
 			    	:key="index"
-			    >
-			      <v-expansion-panel-header>Section - {{ section.name }}</v-expansion-panel-header>
+			    > 
+			      <div>{{section}}</div>
+			      <v-expansion-panel-header>Section {{index + 1}} - {{ section.name }}</v-expansion-panel-header>
 			      <v-expansion-panel-content>
 	            <v-list
 						      subheader
@@ -33,7 +34,10 @@
 						        v-model="settings"
 						        multiple
 						      >
-						        <v-list-item>
+						        <v-list-item
+						        	v-for="(resource, j) in section.resoursces"
+						        	:key="resource.name + j"
+						        >
 						          <template v-slot:default="{ active, toggle }">
 						            <v-list-item-action>
 						              <v-checkbox
@@ -50,56 +54,7 @@
 						          </template>
 						        </v-list-item>
 
-						        <v-list-item>
-						          <template v-slot:default="{ active, toggle }">
-						            <v-list-item-action>
-						              <v-checkbox
-						                v-model="active"
-						                color="primary"
-						                @click="toggle"
-						              ></v-checkbox>
-						            </v-list-item-action>
 
-						            <v-list-item-content>
-						              <v-list-item-title>Sound</v-list-item-title>
-						              <v-list-item-subtitle>Hangouts message</v-list-item-subtitle>
-						            </v-list-item-content>
-						          </template>
-						        </v-list-item>
-
-						        <v-list-item>
-						          <template v-slot:default="{ active, toggle }">
-						            <v-list-item-action>
-						              <v-checkbox
-						                v-model="active"
-						                color="primary"
-						                @click="toggle"
-						              ></v-checkbox>
-						            </v-list-item-action>
-
-						            <v-list-item-content>
-						              <v-list-item-title>Video sounds</v-list-item-title>
-						              <v-list-item-subtitle>Hangouts video call</v-list-item-subtitle>
-						            </v-list-item-content>
-						          </template>
-						        </v-list-item>
-
-						        <v-list-item>
-						          <template v-slot:default="{ active, toggle }">
-						            <v-list-item-action>
-						              <v-checkbox
-						                v-model="active"
-						                color="primary"
-						                @click="toggle"
-						              ></v-checkbox>
-						            </v-list-item-action>
-
-						            <v-list-item-content>
-						              <v-list-item-title>Invites</v-list-item-title>
-						              <v-list-item-subtitle>Notify when receiving invites</v-list-item-subtitle>
-						            </v-list-item-content>
-						          </template>
-						        </v-list-item>
 						      </v-list-item-group>
 						    </v-list>
 		            <v-list
